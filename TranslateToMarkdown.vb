58f31dfb31ac797533219a6fc09f9b404f8f533b
hggorel <78171309+hggorel@users.noreply.github.com>
2022-02-07 22:01:40 -0500

    Update TranslateToMarkdown.vb

--- TranslateToMarkdown.vb
+++ TranslateToMarkdown.vb
@@ -1,9 +1,5 @@
-Sub TranslateToMarkdown()
-'
-' TranslateToMarkdown Macro
-'
+Sub TranslateMarkdown()
 
-'
     Dim cell As Range
     Dim selectedRange As Range
     
@@ -27,100 +23,123 @@ Sub TranslateToMarkdown()
     Dim includeLink As Boolean
     Dim isBold As Boolean
     
+    Dim col As Integer
+    Dim currRow As Integer
+    
+    Dim numColHidden As Integer
+    
     rowCounter = 0
-    For Each Row In selectedRange.Rows
+    numColHidden = 0
+    
+    For Each row In selectedRange.Rows
+        
         colCounter = 0
+        
         thisLine = "|"
         
-        For Each cell In Row.Cells
-            includeLink = False
-            isBold = False
-        
-            '/// Check to see what the color is and if yellow - ignore
+        For Each cell In row.Cells
+            col = cell.Column
+            currRow = cell.row
             
-            Color = cell.Interior.Color
-            checkColor = Hex(Color)
-            
-            colorCompare = StrComp("FFFF", checkColor)
-            
-            '/// Check for hyper link
-            
-            linkCount = cell.Hyperlinks.Count
-            
-            isBold = cell.Font.Bold
-                        
-            If linkCount > 0 Then
-                Dim firstChar As String
-                Dim charCompare As String
-                firstChar = Left$(cell.Value, 1)
-                charCompare = StrComp(firstChar, "!")
+            '///Debug.Print (Columns(col).Hidden)
+            If Columns(col).Hidden = False And Rows(currRow).Hidden = False Then
                 
-                If charCompare = 0 Then
-                    includeLink = False
-                Else
-                    includeLink = True
-                    link = cell.Hyperlinks(1).Address
-                End If
-            End If
+                includeLink = False
+                isBold = False
+        
+                '/// Check to see what the color is and if yellow - ignore
             
-            If (colorCompare = 0) Then
-                thisLine = thisLine & " "
-            Else
+                Color = cell.Interior.Color
+                checkColor = Hex(Color)
+            
+                colorCompare = StrComp("FFFF", checkColor)
+            
+                '/// Check for hyper link
+            
+                linkCount = cell.Hyperlinks.Count
+            
+                isBold = cell.Font.Bold
+                        
+                If linkCount > 0 Then
+                    Dim firstChar As String
+                    Dim charCompare As String
+                    firstChar = Left$(cell.Value, 1)
+                    charCompare = StrComp(firstChar, "!")
+                
+                    If charCompare = 0 Then
+                        includeLink = False
+                    Else
+                        includeLink = True
+                        link = cell.Hyperlinks(1).Address
+                    End If
+                End If
+            
+                If (colorCompare = 0) Then
+                    thisLine = thisLine & " "
+                Else
             
 
             
-                thisLine = thisLine & " "
+                    thisLine = thisLine & " "
                 
-                If includeLink Then
-                    If isBold Then
-                        thisLine = thisLine & "[**"
-                        thisLine = thisLine & cell.Value
-                        thisLine = thisLine & "**]("
+                    If includeLink Then
+                        If isBold Then
+                            thisLine = thisLine & "[**"
+                            thisLine = thisLine & cell.Value
+                            thisLine = thisLine & "**]("
+                        Else
+                            thisLine = thisLine & "["
+                            thisLine = thisLine & cell.Value
+                            thisLine = thisLine & "]("
+                        End If
+                    
+                        thisLine = thisLine & link
+                        thisLine = thisLine & ")"
                     Else
-                        thisLine = thisLine & "["
+                        If isBold Then
+                            thisLine = thisLine & "**"
+                        End If
                         thisLine = thisLine & cell.Value
-                        thisLine = thisLine & "]("
-                    End If
+                        If isBold Then
+                            thisLine = thisLine & "**"
+                        End If
                     
-                    thisLine = thisLine & link
-                    thisLine = thisLine & ")"
-                Else
-                    If isBold Then
-                        thisLine = thisLine & "**"
                     End If
-                    thisLine = thisLine & cell.Value
-                    If isBold Then
-                        thisLine = thisLine & "**"
-                    End If
-                    
-                End If
                 
-                thisLine = thisLine & "|"
-                colCounter = colCounter + 1
+                    thisLine = thisLine & "|"
             End If
-        Next cell
-        
-        Debug.Print thisLine
-        
-        '/// Need to account for the lines to make it a table right after Row 0 is counted
-        
-        If (rowCounter = 0) Then
-            thisLine = "|"
-            colCounter = 0
-            
-            For i = 0 To (totCol - 1)
-                thisLine = thisLine
+            colCounter = colCounter + 1
+            Else
+                numColHidden = numColHidden + 1
                 
-                thisLine = thisLine & "----|"
-                colCounter = colCounter + 1
+            End If
+            Next cell
+            If Rows(currRow).Hidden = False Then
+                Debug.Print thisLine
+            End If
+        
+            '/// Need to account for the lines to make it a table right after Row 0 is counted
+        
+            If (rowCounter = 0) Then
+                thisLine = "|"
+                colCounter = 0
+            
+                For i = 0 To (totCol - 1 - numColHidden)
+                    thisLine = thisLine
                 
-            Next i
+                    thisLine = thisLine & "----|"
+                    colCounter = colCounter + 1
+                Next i
             
-            Debug.Print thisLine
-        End If
+                Debug.Print thisLine
+            End If
         
-        rowCounter = rowCounter + 1
+            rowCounter = rowCounter + 1
         
-    Next Row
-            
+        Next row
+        
+    Debug.Print
+    Debug.Print
+    
+    
 End Sub
